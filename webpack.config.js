082068217d51231746a6171b1e16606ca835ae03
cexbrayat <cedric@ninja-squad.com>
2017-11-04 21:35:48 -0400

    feat: use es2015 module resolution

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,9 @@
 /* global module */
 module.exports = {
   entry: './dist/out-tsc/main.js',
+  resolve: {
+    mainFields: ['es2015', 'browser', 'module', 'main']
+  },
   output: {
     path: __dirname + '/dist',
     filename: 'main.bundle.js'
