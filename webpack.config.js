4c8e63605d4b6dca3202f98ec617ac649d104c2a
cexbrayat <cedric@ninja-squad.com>
2018-04-24 16:16:32 +0200

    chore: bump to webpack 4

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,8 @@
+const webpack = require('webpack');
+
 /* global module */
 module.exports = {
+  mode: 'development',
   entry: './dist/out-tsc/main.js',
   resolve: {
     mainFields: ['es2015', 'browser', 'module', 'main']
@@ -7,5 +10,20 @@ module.exports = {
   output: {
     path: __dirname + '/dist',
     filename: 'main.bundle.js'
-  }
+  },
+  performance: { hints: false },
+  module: {
+    rules: [
+      // Ignore warnings about System.import in Angular
+      { test: /[\/\\]@angular[\/\\].+\.js$/, parser: { system: true } }
+    ]
+  },
+  plugins: [
+    new webpack.ContextReplacementPlugin(
+      // The (\\|\/) piece accounts for path separators in *nix and Windows
+      /\@angular(\\|\/)core(\\|\/)fesm2015/,
+      './src', // location of your src
+      {} // a map of your routes
+    )
+  ]
 };
